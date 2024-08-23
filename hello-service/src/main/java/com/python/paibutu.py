9f5727fa875b942b158dc5e2215cdee74ec64a35
guowei-L55EJ <cguowei@126.com>
2024-08-23 17:15:00 +0800

    python例子

--- hello-service/src/main/java/com/python/paibutu.py
+++ hello-service/src/main/java/com/python/paibutu.py
@@ -5,11 +5,12 @@ if __name__ == "__main__":
     
     headers = {
         'content-type': 'application/json',
-        'Authorization': 'Bearer c2eb45f6fb7a44eca912168ea5747d0c'
+        #'Authorization': 'Bearer 7fc76328a8874255b8d34e32103c0690'
+        'Authorization': 'Bearer 8f3aa3acc32f4c7f9bff9356350dc9e1'
     }
     params = {
-        "projectId": "2235"
+        "projectId": "3882"
     }
     url = 'http://localhost:9098/app-api/sop/chat/calc'
     r = requests.post(url, headers=headers, data=json.dumps(params))
-    print(r)
+    print(r.text)
